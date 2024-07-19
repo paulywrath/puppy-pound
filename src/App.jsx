419f01e57198339031ea592ae4abccfc6e160988
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-07-19 17:18:32 -0400

    Add back button to puppy details

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,7 @@ function App() {
                   <h2>{onePuppy.name}</h2>
                   <h4>Breed: {onePuppy.breed}</h4>
                   <img src={onePuppy.imageUrl} alt="Photo of a puppy" />
+                  <button onClick={() => {setOnePuppy({})}}>Back</button>
                 </> :
 
                 <>
