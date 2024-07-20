608f2290e7c14d4b9370008853e4806fc4018b5b
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-07-20 11:27:13 -0400

    Allow user to log in through nav bar button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { useState } from 'react'
 import PuppyDataAndList from "./PuppyDataAndList"
 
 function App() {
 
+  const [LogInStatus, setLogInStatus] = useState(false)
+
   return (
-            < PuppyDataAndList/>
+            <>
+              <header>
+                <nav>
+                  {
+                    !LogInStatus ? <button onClick={() => {setLogInStatus(true)}}>Log In</button> :
+                    <>
+                      <button onClick={() => {setLogInStatus(false)}}>Log Out</button>
+                      <button>My Account</button>
+                    </>
+                  }
+                </nav>
+              </header>
+              <main>
+                <PuppyDataAndList LogInStatus={LogInStatus}/>
+              </main>
+            </>
           )
 }
 
