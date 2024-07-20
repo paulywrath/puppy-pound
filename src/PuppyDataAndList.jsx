a86222b4681dcd4f224c71b8980a3204ce83e1a2
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-07-20 10:33:21 -0400

    Put get-and-list-puppies component in its own file

---
+++ src/PuppyDataAndList.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react'
+import PuppyDetails from './PuppyDetails';
+
+function PuppyDataAndList() {
+
+  const [puppies, setPuppies] = useState([]);
+
+  useEffect( () => {
+    const getPuppies = async() => {
+      try {
+        const response = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2406-FTB-ET-WEB-FT/players`);
+        const result = await response.json();
+        const playersfromAPI = result.data.players;
+        setPuppies(playersfromAPI);
+      } catch (e) {
+        alert(e);
+      }
+    }
+    getPuppies();
+  },[])
+
+  const [onePuppy, setOnePuppy] = useState({})
+  
+  const getPuppyDetails = async(puppyID) => {
+    try {
+      const response = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2406-FTB-ET-WEB-FT/players/${puppyID}`);
+      const result = await response.json();
+      const puppyFromAPI = result.data.player
+      setOnePuppy(puppyFromAPI);
+    } catch (e) {
+      alert(e);
+    }
+  }
+
+  return (
+            <>
+              {onePuppy.name ? 
+                < PuppyDetails onePuppy={onePuppy} setOnePuppy={setOnePuppy}/> :
+
+                <>
+                  <h1>Puppies for Adoption</h1>
+                  
+                  <ul>
+                  {
+                    puppies.map((puppy) => {
+                      return (
+                        <>
+                        <li onClick={() => { getPuppyDetails(puppy.id)}}>
+                        <h3>{puppy.name}</h3>
+                        <img src={puppy.imageUrl} alt="Photo of a puppy" />
+                        </li>
+                        </>
+                      )
+                    })
+                  }
+                  </ul>
+                </>
+              }
+            </>
+          )
+}
+
+export default PuppyDataAndList
