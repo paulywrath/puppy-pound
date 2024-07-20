d9703c57dc1f703cd09fe22399b9471502296820
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-07-20 11:56:26 -0400

    Add "Adopt Now" btn to puppy details if logged in

--- src/PuppyDetails.jsx
+++ src/PuppyDetails.jsx
@@ -1,10 +1,13 @@
-function PuppyDetails( { onePuppy , setOnePuppy } ) {
+function PuppyDetails( { onePuppy , setOnePuppy, LogInStatus } ) {
 
   return (
           <>
             <h2>{onePuppy.name}</h2>
             <h4>Breed: {onePuppy.breed}</h4>
             <img src={onePuppy.imageUrl} alt="Photo of a puppy" />
+
+            { LogInStatus === true ? <button>Adopt Now</button>: null}
+            
             <button onClick={() => {setOnePuppy({})}}>Back</button>
           </>
           )
